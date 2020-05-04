9ff1fdf813ae16de6b4e9f07c7572668dfd59dea
adrifunes <61049987+adrifunes@users.noreply.github.com>
2020-05-04 18:06:15 +0200

    Update MENU ENGINE FAILURE CORPORATION.c

--- MENU ENGINE FAILURE CORPORATION.c
+++ MENU ENGINE FAILURE CORPORATION.c
@@ -1,43 +1,17 @@
-#include <stdio.h>
-
-void main ()
+#include<stdio.h>
+typedef struct
 {
-int op;
-char r;
-do
+	char nickname[100];
+	char correo[100];
+	char nombre[100];
+	char apellido[100];
+} Usuario;
+
+typedef struct
 {
-	/* Este proyecto tiene como función darte a conocer diferentes
-caracteristicas tecnicas de vehiculos concretos */
-printf("Bienvenido a nuestro programa 'Engine Failure Corporation' \n");
-
-
-printf("Seleccione el tipo de vehiculo que desea buscar:\n1)Coche\n2)Moto\n3)Furgoneta\n4)Camion\n5)Autobus\n");
-  do
-  {
-  scanf("%i",&op);
-  }
-  while(op<1||op>6);
-  switch(op)
-    {
-    case 1:
-      printf("1)Coche\n");
-      break;
-    case 2:
-      printf("2)Moto\n");
-      break;
-    case 3:
-      printf("3)Furgoneta\n");
-      break;
-    case 4:
-      printf("4)Camion\n");
-      break;
-	case 5:
-      printf("5)Autobus\n");
-      break;
-      //En un futuro el programa preguntará ciertas características del vehículo para poder concretar un modelo y entregará la información correspondiente del mismo.
-	}
-    printf("Desea buscar otro vehiculo? Pulse 'r'\n");
-    scanf("%s",&r);
-}
-while(r=='r');
-}
+	char tipo[100];
+	char marca[100];
+	char modelo[100];
+	char carroceria[100];
+	char motor[100];
+} Vehiculo;
